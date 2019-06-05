b90c5fc7d11f5835f1d8a884275064edba27abe6
thato <thatokekana77@gmail.com>
2019-06-05 12:19:06 +0200

    some changes

--- src/main/java/za/co/pointmagnificent/test/Application.java
+++ src/main/java/za/co/pointmagnificent/test/Application.java
@@ -11,6 +11,8 @@ public class Application{
 public static void main (String [] args){
 Scanner scanner = new Scanner(System.in);
 Date date = new Date();
+Cashier cashier = new Cashier();
+Manager manager = new Manager();
 Sales sales = new Sales();
 Product product = new Product();
 Product p1 = new Product();
@@ -27,15 +29,15 @@ p1.timeManufactured=0.0;
 p1.dateManufactured=null;
 p1.bestBefore=null;
 p1.barcode=123;
-p1.productDescription="Eggs ritebrand 30 large";
+p1.productDescription="EGGS RITEBRAND LARGE 30s";
 
 
-p2.retailPrice=14.99;
+p2.retailPrice=(14.99);
 p2.timeManufactured=0.0;
 p2.dateManufactured=null;
 p2.bestBefore=null;
 p2.barcode=1234;
-p2.productDescription="Milk ritebrand 1 LITRE full cream";
+p2.productDescription="MILK RITEBRAND 1l F/CREAM";
 
 
 p3.retailPrice=13.99;
@@ -43,11 +45,11 @@ p3.timeManufactured=0.0;
 p3.dateManufactured=null;
 p3.bestBefore=null;
 p3.barcode=12345;
-p3.productDescription="Bread Albany Brown 600g";
+p3.productDescription="BROWN BREAD SUPERIOR 700G";
 
 System.out.println("\t\t\t\t>>>>>>>WELCOME TO POINTMAGNIFICENT SUPERMARKET>>>>>>>>>>>>>>>>");
-System.out.println(" \t\t\t\t\t     763,madison view, Pretoria South Africa.");
-System.out.println("	   \t\t\t\t\t  Tel: 012 357 9856, Fax: 012 558 6698.");
+System.out.println(" \t\t\t\t\t     763 Madison View, Pretoria, South Africa");
+System.out.println("	   \t\t\t\t\t  Tel: 012 357 9856, Fax: 012 558 6698");
 System.out.println("               \t\t\t\t\t   Email: info@pointmagnificent.com");
 System.out.println("");
 
@@ -75,7 +77,7 @@ System.out.println("\t\tPLEASE FILL-IN YOUR DETAILS BELOW TO CONTINUE AS CASHIER
                   scanner.nextLine();
 
 System.out.println("ENTER NAME:");
-String Name = scanner.nextLine();
+String name = scanner.nextLine();
 
 System.out.println("ENTER SURNAME:");
 String Surname = scanner.nextLine();
@@ -120,10 +122,10 @@ System.out.println("\t\t\t\t\tplease Wait while we process your Details........"
 }
   
   else if (password.equals(password) && Confirm.equals(password)) {
-        System.out.println("\t\t\t\tPASSWORD DO NOT MATCH PLEASE TRY AGAIN!!!!!!");
+        System.out.println("\t\t\t\tPASSWORD DOES NOT MATCH PLEASE TRY AGAIN!!!!!!");
     
  } else {
-        System.out.println("\t\t\t\tPASSWORD DO NOT MATCH PLEASE TRY AGAIN!!!!!!");
+        System.out.println("\t\t\t\tPASSWORD DOES NOT MATCH PLEASE TRY AGAIN!!!!!!");
     }
 
 
@@ -155,7 +157,7 @@ System.out.println("CONFIRM PASSWORD: *********");
 //System.out.println("NEW EMPLOYEE NUMBER :");
 
 
-System.out.println("\t\t\t\t\tCONGRATULATION YOUR REGISTRATION IS COMPLETED.........");
+System.out.println("\t\t\t\t\tCONGRATULATIONS! YOUR REGISTRATION IS COMPLETED.........");
                         scanner.nextLine();            
 
 System.out.println("\t\t\t\t\t\t\t........WELCOME : "+ Surname);
@@ -242,16 +244,16 @@ if(press ==1){
         long bc1 = scanner.nextLong();
 
  if(bc1 == p1.barcode){
- System.out.println(p1.productDescription + "R" + p1.retailPrice);
+ System.out.println(p1.productDescription + "R" + p1.retailPrice*p1.quantity);
  inventories[0]=null;
 }
 else if (bc1 == p2.barcode){
-System.out.println(p2.productDescription + "R" + p2.retailPrice);
+System.out.println(p2.productDescription + "R" + p2.retailPrice*p2.quantity);
 inventories[1]=null;
 }
 
 else if(bc1 == p3.barcode){
-System.out.println(p3.productDescription + "R" + p3.retailPrice);
+System.out.println(p3.productDescription + "R" + p3.retailPrice*p2.quantity);
 inventories[2]=null;
 }
 System.out.println("..........................");
@@ -260,16 +262,16 @@ System.out.println("Enter Barcode");
         long bc2 = scanner.nextLong();
 
  if(bc2 == p1.barcode){
- System.out.println(p1.productDescription + "R" + p1.retailPrice);
+ System.out.println(p1.productDescription + "R" + p1.retailPrice*p1.quantity);
  inventories[0]=null;
 }
 else if (bc2 == p2.barcode){
-System.out.println(p2.productDescription + "R" + p2.retailPrice);
+System.out.println(p2.productDescription + "R" + p2.retailPrice*p2.quantity);
 inventories[1]=null;
 }
 
 else if(bc2 == p3.barcode){
-System.out.println(p3.productDescription + "R" + p3.retailPrice);
+System.out.println(p3.productDescription + "R" + p3.retailPrice*p3.quantity);
 inventories[2]=null;
 }
 
@@ -279,23 +281,26 @@ System.out.println("Enter Barcode");
         long bc3 = scanner.nextLong();
 
  if(bc3 == p1.barcode){
- System.out.println(p1.productDescription + "R" + p1.retailPrice);
+ System.out.println(p1.productDescription + "R" + p1.retailPrice*p1.quantity);
  inventories[0]=null;
 }
 else if (bc3 == p2.barcode){
-System.out.println(p2.productDescription + "R" + p2.retailPrice);
+System.out.println(p2.productDescription + "R" + p2.retailPrice*p2.quantity);
 inventories[1]=null;
 }
 
 else if(bc3 == p3.barcode){
-System.out.println(p3.productDescription + "R" + p3.retailPrice);
+System.out.println(p3.productDescription + "R" + p3.retailPrice*p3.quantity);
 inventories[2]=null;
 }
 
-double total = p1.retailPrice + p2.retailPrice + p3.retailPrice;
-System.out.println(total);
+sales.total = p1.retailPrice*p1.quantity + p2.retailPrice*p2.quantity + p3.retailPrice*p3.quantity;
+System.out.println(sales.total);
 
 System.out.println("..........................");
+
+System.out.print("DO TO YOU WANT TO VOID AN ITEM? PRESS 2 TO VOID: ");
+int 
 
 System.out.println("enter cash amount");
 sales.cash=scanner.nextDouble();
@@ -303,28 +308,28 @@ sales.cash=scanner.nextDouble();
 
 
 
-double change = sales.cash - total;
+sales.change = sales.cash - sales.total;
 
 
-sales.vat=p2.retailPrice*0.15 + p3.retailPrice*0.15;
+sales.vat=p1.retailPrice*p1.quantity*0.15 + p2.retailPrice*p2.quantity*0.15;
 
 System.out.println();//skip 2 lines
 System.out.println(">>>>>>>>>>>>POINTMAGNIFICENT>>>>>>>>>>>>>>>>>>>>>>>>>>");
 
-System.out.println("\t******Madinson view********");
+System.out.println("\t******Madison View********");
 scanner.nextLine();
 System.out.println("\t\tTel:012 357 9856.  ");
 System.out.println("\t\tFax:012 357 9856. ");
 System.out.println("\t\tVat:4540149558.");
 System.out.println("\t\t***GAU200422C***");
 System.out.println();
-System.out.println(p1.productDescription + "R" + p1.retailPrice);
-System.out.println(p2.productDescription + "R" +  p2.retailPrice);
-System.out.println(p3.productDescription + "R" +  p3.retailPrice);
+System.out.println(p1.productDescription + "R" + p1.retailPrice*p1.quantity);
+System.out.println(p2.productDescription + "R" +  p2.retailPrice*p2.quantity);
+System.out.println(p3.productDescription + "R" +  p3.retailPrice*p3.quantity);
 
-System.out.println("TOTAL R" + total);
+System.out.println("TOTAL R" + sales.total);
 System.out.println("CASH R" + sales.cash);
-System.out.println("CHANGE R" + change);
+System.out.println("CHANGE R" + sales.change);
 System.out.println("VAT R" + sales.vat);
 
 System.out.println("........................TAX INVOICE............................");
