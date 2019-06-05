3e5a388308d1bfd6552ed26b3a062ea225af8335
thato <thatokekana77@gmail.com>
2019-06-05 11:01:21 +0200

    edited primary package declarations

---
+++ src/main/java/za/co/pointmagnificent/primary/Manufacturer.java
@@ -0,0 +1,9 @@
+package za.co.pointmagnificent.primary;
+
+public class Manufacturer extends Provider{
+
+public void createProduct(){
+System.out.print("Beans");
+}
+
+}
---
+++ src/main/java/za/co/pointmagnificent/primary/Supplier.java
@@ -0,0 +1,9 @@
+package za.co.pointmagnificent.primary;
+
+public class Supplier extends Provider{
+
+public void createProduct(){
+System.out.print("Bread");
+}
+
+}
